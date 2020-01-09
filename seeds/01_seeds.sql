cb7b471e745adc3699328b799ff979b5a2623cd5
7 <swistek.labs@gmail.com>
2020-01-09 22:16:57 +0000

    property reviews

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -11,17 +11,45 @@
 --       ('Shine twenty', 'nice location', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=350' , 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg' , 13644 , 1 , 7 , 8 , 'Canada' , '340 Dokto Park' , 'Upfufa' , 'Nova Scotia' , 29045 , true),
 --       ('Game fill', 'nice location', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=350' , 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg' , 23428 , 5 , 6 , 4 , 'Canada' , '834 Buwmi Road' , 'Rotunif' , 'Newfoundland And Labrador' , 58224 , true)
 
-INSERT INTO users
-  (name, email, password)
-    VALUES ('Eva Stanley' , 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-    VALUES ('Louisa Meyer' , 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-    VALUES ('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-    VALUES ('Sue Luna', 'jasonvincent@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-    VALUES ('Rosalie Garza', 'jacksondavid@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-    VALUES ('Etta West', 'charlielevy@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-    VALUES ('Margaret Wong', 'makaylaweiss@icloud.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-    VALUES ('Leroy Hart', 'jaycereynolds@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
-    
+-- INSERT INTO users
+--   (name, email, password)
+--     VALUES ('Eva Stanley' , 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+--       ('Louisa Meyer' , 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+--       ('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+--       ('Sue Luna', 'jasonvincent@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+--       ('Rosalie Garza', 'jacksondavid@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+--       ('Etta West', 'charlielevy@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+--       ('Margaret Wong', 'makaylaweiss@icloud.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+--       ('Leroy Hart', 'jaycereynolds@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+ `start_date` |  `end_date`  | `property_id` | `guest_id` 
+--------------|--------------|---------------|----------
+ 2018-09-11   | 2018-09-26   |           313 |      399
+ 2019-01-04   | 2019-02-01   |           515 |      628
+ 2021-10-01   | 2021-10-14   |           217 |       93
+ 2014-10-21   | 2014-10-21   |           557 |       71
+ 2016-07-17   | 2016-08-01   |           672 |      794
+ 2018-05-01   | 2018-05-27   |           285 |      160
+ 2022-10-04   | 2022-10-23   |            65 |      120
+ 2015-09-13   | 2015-09-30   |           614 |      419
+ 2023-05-27   | 2023-05-28   |           484 |      661
+ 2023-04-23   | 2023-05-02   |           770 |       71
+
+INSERT INTO reservations
+  (start_date, end_date, property_id, guest_id)
+
+
+
+
+
+INSERT INTO property_reviews
+  (guest_id, property_id, reservation_id, rating, message)
+    VALUES (2, 5, 10, 3, 'message'),
+           (1, 4, 1, 4, 'message'),
+           (8, 1, 2, 4, 'message'),
+           (3, 8, 5, 4, 'message'),
+           (4, 2, 7, 5, 'message'),
+           (4, 3, 4, 4, 'message'),
+           (5, 6, 3, 5, 'message')    
 
 
 
